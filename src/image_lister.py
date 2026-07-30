95c63f5afa4709540067d304766ff401fe9da825
Hrish <rajeevhrishabh@gmail.com>
2026-07-30 23:12:07 +0200

    refactored image lister to separate module

--- src/image_lister.py
+++ src/image_lister.py
@@ -6,11 +6,10 @@ from sentinelhub import (
     MosaickingOrder,
     SentinelHubCatalog,
     SentinelHubRequest,
-    SHConfig,
     bbox_to_dimensions,
 )
 
-from config import CDSE_CLIENT_ID, CDSE_CLIENT_SECRET
+from config_creator import main as create_config
 
 
 def get_available_acquisitions(bbox, start_date, end_date, max_cloud_cover, config):
@@ -30,37 +29,9 @@ def get_available_acquisitions(bbox, start_date, end_date, max_cloud_cover, conf
     return list(search)
 
 
-# ------------------------------------------------------------------
-# Copernicus Data Space configuration
-# ------------------------------------------------------------------
-
-
-def get_config() -> SHConfig:
-    TOKEN_URL = "https://identity.dataspace.copernicus.eu/auth/realms/CDSE/protocol/openid-connect/token"
-    BASE_URL = "https://sh.dataspace.copernicus.eu"
-    """
-    Reads credentials from environment variables.
-    Required:
-        CDSE_CLIENT_ID
-        CDSE_CLIENT_SECRET
-    """
-    config = SHConfig()
-    client_id = CDSE_CLIENT_ID
-    client_secret = CDSE_CLIENT_SECRET
-
-    config.sh_client_id = client_id
-    config.sh_client_secret = client_secret
-    config.sh_base_url = BASE_URL
-    config.sh_token_url = TOKEN_URL
-
-    config.save("cdse_temp")
-
-    return SHConfig("cdse_temp")
-
-
 def main(bbox, start_date, end_date, max_cloud_cover):
 
-    config = get_config()
+    config = create_config()
     bbox_obj = BBox(bbox=bbox, crs=CRS.WGS84)
 
     acquisitions = get_available_acquisitions(
--- src/main.py
+++ src/main.py
@@ -5,7 +5,7 @@ from image_processor import main as process_image
 def main() -> None:
     bbox = (31.1482, -29.5482, 31.2075, -29.5882)
     start_date = "2024-01-01"
-    end_date = "2024-01-31"
+    end_date = "2025-01-01"
 
     list_image(
         bbox=bbox,

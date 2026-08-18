d7ec13c46ac025c4f8e75add7efe29e0d7be0650
Hrish <rajeevhrishabh@gmail.com>
2026-08-18 09:18:33 +0200

    Refactored and removed main wrapper

--- src/river_extracter/image_lister_offline.py
+++ src/river_extracter/image_lister_offline.py
@@ -1,14 +1,38 @@
 from pathlib import Path
 
 
-def get_avaliable_acquisitions(path: str) -> list:
+def list_offline_images(
+    directory: str | Path,
+) -> list:
+    """
+    Return filenames of downloaded images in the downloads folder
 
-    os_path = Path(path)
-    files = [f.name for f in os_path.rglob("*") if f.is_file()]
+    Parameters:
 
-    return files
+    directory:
+        Where to look for the files
+
+    recursive:
+        If true then this will search subdirectories
+
+    Returns list[str]:
+        Stored list of GeoTIFF image filenames
+
+    Raises:
+        FileNotFoundError
+            If the files does not exist
+
+        NotADirectoryError
+            If the directory is not a directory
 
 
-def main(path: str) -> list:
-    acquisitions = get_avaliable_acquisitions(path)
-    return acquisitions
+    """
+    directory = Path(directory)
+
+    if not directory.exists():
+        raise FileNotFoundError(f"File does not exist: {directory}")
+
+    if not directory.is_dir():
+        raise NotADirectoryError(f"Acquisition path is not a directory: {directory}")
+
+    return sorted(path.name for path in directory.glob("*.tif") if path.is_file())
